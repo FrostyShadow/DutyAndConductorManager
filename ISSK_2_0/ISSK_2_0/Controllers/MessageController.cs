7028e185bca61bf0d251bb9454be11012cb8aa65
Filip Kosmala <kosmala.filip@gmail.com>
2019-05-18 22:04:59 +0200

    Updated bootstrap and jQuery

---
+++ ISSK_2_0/ISSK_2_0/Controllers/MessageController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ISSK_2_0.Controllers
+{
+    public class MessageController : Controller
+    {
+        // GET: Message
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ISSK_2_0/ISSK_2_0/Controllers/VehicleController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ISSK_2_0.Controllers
+{
+    public class VehicleController : Controller
+    {
+        // GET: Vehicle
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
